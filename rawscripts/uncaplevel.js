266ddeca539aed7ee2d52c136eec6f9efae2e473
Sudzy <167475761+sudzythegoat@users.noreply.github.com>
2024-08-22 14:14:54 -0500

    Update uncaplevel.js

--- rawscripts/uncaplevel.js
+++ rawscripts/uncaplevel.js
@@ -4,7 +4,7 @@ if (userInput !== null) {
     let targetDiv = document.querySelector('.sc-biLNch.jcrWHW');
 
     if (targetDiv) {
-        targetDiv.innerHTML = `Level ${userInput} <br><small style="font-size: 10px;">uncapped with Midnight Injector</small>`;
+        targetDiv.innerHTML = `Level ${userInput} <br><small style="font-size: 10px;">uncapped with Gimware</small>`;
     } else {
         console.error("Element not found, please check query selector");
     }
